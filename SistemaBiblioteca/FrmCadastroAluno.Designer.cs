f4748a62fea0d4d7b6e4613fb45812192dd2e585
matheushenrique1998 <134648358+matheushenrique1998@users.noreply.github.com>
2023-11-07 13:46:06 -0300

    como

---
+++ SistemaBiblioteca/FrmCadastroAluno.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace SistemaBiblioteca
+{
+    partial class FrmCadastroAluno
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtPesquisar = new System.Windows.Forms.TextBox();
+            this.dgvAlunos = new System.Windows.Forms.DataGridView();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.txtDataNascimento = new System.Windows.Forms.MaskedTextBox();
+            this.txtCPF = new System.Windows.Forms.MaskedTextBox();
+            this.txtTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.txtCelular = new System.Windows.Forms.MaskedTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAlunos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(107, 30);
+            this.txtNome.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(441, 30);
+            this.txtNome.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(593, 37);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CPF";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(40, 86);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "E-mail";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(108, 86);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(4);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(440, 30);
+            this.txtEmail.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(572, 94);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Data de Nascimento";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(19, 145);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Telefone";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(352, 153);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Celular";
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdicionar.Location = new System.Drawing.Point(23, 215);
+            this.btnAdicionar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(129, 42);
+            this.btnAdicionar.TabIndex = 12;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(593, 226);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 20);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Cod. Cliente";
+            // 
+            // txtPesquisar
+            // 
+            this.txtPesquisar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPesquisar.Location = new System.Drawing.Point(707, 219);
+            this.txtPesquisar.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPesquisar.Name = "txtPesquisar";
+            this.txtPesquisar.Size = new System.Drawing.Size(117, 30);
+            this.txtPesquisar.TabIndex = 16;
+             this.txtPesquisar.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtPesquisar_KeyUp);
+            // 
+            // dgvAlunos
+            // 
+            this.dgvAlunos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAlunos.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvAlunos.Location = new System.Drawing.Point(23, 293);
+            this.dgvAlunos.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvAlunos.Name = "dgvAlunos";
+            this.dgvAlunos.RowHeadersWidth = 51;
+            this.dgvAlunos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvAlunos.Size = new System.Drawing.Size(952, 246);
+            this.dgvAlunos.TabIndex = 18;
+           this.dgvAlunos.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvAlunos_CellMouseDoubleClick);
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAtualizar.Location = new System.Drawing.Point(160, 215);
+            this.btnAtualizar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(129, 42);
+            this.btnAtualizar.TabIndex = 19;
+            this.btnAtualizar.Text = "Atualizar";
+            this.btnAtualizar.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluir.Location = new System.Drawing.Point(297, 215);
+            this.btnExcluir.Margin = new System.Windows.Forms.Padding(4);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(129, 42);
+            this.btnExcluir.TabIndex = 20;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPesquisar.Location = new System.Drawing.Point(845, 215);
+            this.btnPesquisar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(129, 42);
+            this.btnPesquisar.TabIndex = 21;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // txtDataNascimento
+            // 
+            this.txtDataNascimento.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDataNascimento.Location = new System.Drawing.Point(741, 87);
+            this.txtDataNascimento.Mask = "00/00/0000";
+            this.txtDataNascimento.Name = "txtDataNascimento";
+            this.txtDataNascimento.Size = new System.Drawing.Size(232, 30);
+            this.txtDataNascimento.TabIndex = 22;
+            this.txtDataNascimento.ValidatingType = typeof(System.DateTime);
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCPF.Location = new System.Drawing.Point(643, 34);
+            this.txtCPF.Mask = "000\\.000\\.000-00";
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(332, 30);
+            this.txtCPF.TabIndex = 23;
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTelefone.Location = new System.Drawing.Point(108, 143);
+            this.txtTelefone.Mask = "(00) 0000-0000";
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(237, 30);
+            this.txtTelefone.TabIndex = 24;
+            // 
+            // txtCelular
+            // 
+            this.txtCelular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCelular.Location = new System.Drawing.Point(421, 143);
+            this.txtCelular.Mask = "(00) 00000-0000";
+            this.txtCelular.Name = "txtCelular";
+            this.txtCelular.Size = new System.Drawing.Size(237, 30);
+            this.txtCelular.TabIndex = 25;
+            // 
+            // FrmCadastroAluno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1007, 559);
+            this.Controls.Add(this.txtCelular);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.txtCPF);
+            this.Controls.Add(this.txtDataNascimento);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.dgvAlunos);
+            this.Controls.Add(this.txtPesquisar);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmCadastroAluno";
+            this.Text = "Cadastro de Aluno";
+            this.Load += new System.EventHandler(this.FrmCadastroAluno_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAlunos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtPesquisar;
+        private System.Windows.Forms.DataGridView dgvAlunos;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.MaskedTextBox txtDataNascimento;
+        private System.Windows.Forms.MaskedTextBox txtCPF;
+        private System.Windows.Forms.MaskedTextBox txtTelefone;
+        private System.Windows.Forms.MaskedTextBox txtCelular;
+    }
+}
+
---
+++ SistemaBiblioteca/FrmCadastroLivro.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SistemaBiblioteca
+{
+    public partial class FrmCadastroLivro : Form
+    {
+        public string conexaoString;
+        private SqlConnection conexaoDB;
+        DataGridViewRow linhaSelecionada;
+        public FrmCadastroLivro()
+        {
+            InitializeComponent();
+            conexaoString = "Data Source=MAR0625650W10-1;Initial Catalog=Biblioteca;Integrated Security=True";
+
+            conexaoDB = new SqlConnection(conexaoString);
+        }
+
+
+        private void carregarDadosLivros(int id = 0)
+        {
+            try
+            {
+                string sql = "select * from livros";
+
+                conexaoDB.Open();
+
+                if (id == 0)
+                {
+                    sql = "SELECT * FROM livros";
+                }
+                else
+                {
+                    sql = "SELECT * FROM livros WHERE id=" + id;
+                }
+
+                SqlDataAdapter adapter = new SqlDataAdapter(sql, conexaoDB);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                dataTable.Columns["numero_pagina"].ColumnName = "N° Página";
+                dataTable.Columns["preco"].ColumnName = "Preço";
+                dataTable.Columns["ano_publicacao"].ColumnName = "Ano publicação";
+
+                dgvLivro.DataSource = dataTable;
+
+                conexaoDB.Close();
+
+            } catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao carregar os dados: " + ex);
+            }
+
+        }
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sql = "INSERT INTO livros(titulo, autor, numero_pagina, preco, ano_publicacao, isbn) VALUES (@titulo, @autor, @numero_pagina, @preco, @ano_publicacao, @isbn)";
+
+                conexaoDB.Open();
+
+                SqlCommand sqlCmd = new SqlCommand(sql, conexaoDB);
+
+                sqlCmd.Parameters.AddWithValue("@titulo", txtTitulo.Text);
+                sqlCmd.Parameters.AddWithValue("@autor", txtAutor.Text);
+                sqlCmd.Parameters.AddWithValue("@numero_pagina", Convert.ToInt32(txtNumeroPagina.Text));
+                sqlCmd.Parameters.AddWithValue("@preco", Convert.ToDecimal(txtPreco.Text));
+                sqlCmd.Parameters.AddWithValue("@ano_publicacao", Convert.ToInt32(txtAnoPublicacao.Text));
+                sqlCmd.Parameters.AddWithValue("@isbn", txtISBN.Text);
+
+
+                sqlCmd.ExecuteNonQuery();
+
+                MessageBox.Show("Cadastro realizado com sucesso!");
+
+                conexaoDB.Close();
+
+                carregarDadosLivros();
+
+            } catch (SqlException ex) {
+                MessageBox.Show("Erro ao Inserir os Dados: " + ex);
+            }
+        }
+
+        private void dgvLivro_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                linhaSelecionada = dgvLivro.Rows[e.RowIndex];
+
+                txtISBN.Text = linhaSelecionada.Cells["isbn"].Value.ToString();
+                txtTitulo.Text = linhaSelecionada.Cells["titulo"].Value.ToString();
+                txtPreco.Text = linhaSelecionada.Cells["Preço"].Value.ToString();
+                txtAnoPublicacao.Text = linhaSelecionada.Cells["Ano publicação"].Value.ToString();
+                txtNumeroPagina.Text = linhaSelecionada.Cells["N° Página"].Value.ToString();
+                txtAutor.Text = linhaSelecionada.Cells["autor"].Value.ToString();
+
+                btnAdicionar.Enabled = false;
+                btnAtualizar.Enabled = true;
+                btnExcluir.Enabled = true;
+            }
+        }
+        private void FrmCadastroLivro_Load(object sender, EventArgs e)
+        {
+            carregarDadosLivros();
+        }
+
+        private void btnAtualizar_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                int id = Convert.ToInt32(linhaSelecionada.Cells["id"].Value.ToString());
+
+                string sql = "UPDATE livros SET " +
+                    "titulo=@titulo, " +
+                    "autor=@autor, " +
+                    "numero_pagina=@numero_pagina, " +
+                    "preco=@preco, " +
+                    "ano_publicacao=@ano_publicacao, " +
+                    "isbn=@isbn " +
+                    "WHERE id=@id";
+
+                conexaoDB.Open();
+
+                SqlCommand sqlCmd = new SqlCommand(sql, conexaoDB);
+
+                sqlCmd.Parameters.AddWithValue("@id", id);
+                sqlCmd.Parameters.AddWithValue("@titulo", txtTitulo.Text);
+                sqlCmd.Parameters.AddWithValue("@autor", txtAutor.Text);
+                sqlCmd.Parameters.AddWithValue("@numero_pagina", txtNumeroPagina.Text);
+                sqlCmd.Parameters.AddWithValue("@preco", txtPreco.Text);
+                sqlCmd.Parameters.AddWithValue("@ano_publicacao", txtAnoPublicacao.Text);
+                sqlCmd.Parameters.AddWithValue("@isbn", btnPesquisar.Text);
+
+                sqlCmd.ExecuteNonQuery();
+
+                MessageBox.Show("Atualização Realizada com Sucesso!!!");
+
+                conexaoDB.Close();
+
+                carregarDadosLivros();
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Erro ao Atualizar os Dados: " + ex);
+            }
+        }
+
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if(linhaSelecionada != null)
+            {
+                DialogResult resultado = MessageBox.Show("Tem certeza que deseja exlcuir o Livro","Atenção",MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if(resultado == DialogResult.Yes)
+                {
+
+                    try
+                    {
+                        int id = Convert.ToInt32(linhaSelecionada.Cells["id"].Value.ToString());
+
+                        string sql = "DELETE FROM livros " +
+                                     "WHERE id=@id";
+
+                        conexaoDB.Open();
+
+                        SqlCommand sqlCmd = new SqlCommand(sql, conexaoDB);
+
+                        sqlCmd.Parameters.AddWithValue("@id", id);
+                        sqlCmd.ExecuteNonQuery();
+
+                        MessageBox.Show("livro excluido com Sucesso!!!");
+
+                        conexaoDB.Close();
+
+                        carregarDadosLivros();
+
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show("Erro ao Excluir os Dados: " + ex);
+                    }
+
+
+                }
+            }
+        }
+
+        private void btnPesquisar_Click_1(object sender, EventArgs e)
+        {
+            int id;
+            if (int.TryParse(txtPesquisar.Text, out id))
+            {
+                carregarDadosLivros(id);
+            }
+            else
+            {
+                MessageBox.Show("Código do Livro inválido");
+            }
+        
+        }
+
+        private void txtPesquisar_TextChanged(object sender, EventArgs e)
+        {
+            carregarDadosLivros();
+        }
+    }
+}
